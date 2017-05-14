5b71a2ffb6a5072fd8c06181aad2d03f285c1288
dhrubesh <dhrubesh97@gmail.com>
2017-05-15 00:45:40 +0530

    added mapbox

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
-function test(){
-  console.log("cool");
-}
+mapboxgl.accessToken = 'pk.eyJ1IjoiZGhydWJlc2hoIiwiYSI6ImNqMnAyMWJydDAyaWQycXBoNWo0YmF1dnkifQ.YQSKbXX4vPTiVQTnsk0S7w';
+var map = new mapboxgl.Map({
+container: 'map',
+style: 'mapbox://styles/mapbox/dark-v9',
+center: [78.962880, 20.593684],
+  zoom: 4,
+  hash: true,
+  renderWorldCopies: true,
+  trackResize: true
+});
